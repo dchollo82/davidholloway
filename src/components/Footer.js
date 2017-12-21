21b32f2b11b1566142db6ff957fc6b7d898d2071
David Holloway <david.holloway@uk.tesco.com>
2017-12-21 16:09:17 +0000

    Footer and social icons constructed; Not really liking the length of these BEM names. May revisit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,16 @@ import React from 'react';
 class Footer extends React.Component {
     render() {
       return (
-        
-          <p>&copy; David Holloway</p>          
-       
-        
+        <div className="footer">
+          <p className="footer__copyright">&copy; David Holloway</p>
+          <ul className="footer__social__icons">
+            <li className="footer__social__icon footer__social__icon--github">Github</li>
+            <li className="footer__social__icon footer__social__icon--linkedin">LinkedIn</li>
+            <li className="footer__social__icon footer__social__icon--codepen">Codepen</li>
+            <li className="footer__social__icon footer__social__icon--twitter">Twitter</li>
+            <li className="footer__social__icon footer__social__icon--instagram">Instagram</li>
+          </ul>
+        </div>
       )
     }
   }
