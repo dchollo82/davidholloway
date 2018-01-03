bb50bfcc070b0b5effec2e48ca51ae173cbb7bed
David Holloway <davidholloway06@gmail.com>
2018-01-03 14:52:35 +0000

    Changed intro content around

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,9 +5,9 @@ class Intro extends React.Component {
       return (
           
         <div className="intro">
-            <h1 className="intro__heading">Hi, I'm David Holloway</h1>            
+            <h1 className="intro__heading">Hi, there</h1>            
             <div className="intro__desc">
-                <p className="intro__tagline">A UI Developer from London, UK.</p>
+                <p className="intro__tagline">I'm David Holloway. A UI Developer from London, UK.</p>
                 <p className="intro__copy">I'm Passionate about delightful, usable experiences made possible by great reusable and scalable code.</p>                
             </div>                     
         </div>            
